f17dca6c1129241064b2e542c06b398fb58f6300
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2022-12-05 11:59:47 -0500

    refactor elf_calories code to separate module

---
+++ elf_calories.py
@@ -0,0 +1,25 @@
+def load_elf_cals(fname):
+    """Build the list of elf carrying calories"""
+    ecal = []
+    count = 0
+    with open('data/' + fname) as inf:
+        content = inf.read().splitlines()
+        for line in content:
+            if len(line) == 0:
+                ecal.append(count)
+                count = 0
+            else:
+                count += int(line)
+        if count > 0:
+            ecal.append(count)
+    return ecal
+
+
+def elf_max_calories(fname):
+    """Find the elf carrying the most calories and report how many he has"""
+    elf_cals = load_elf_cals(fname)
+    print(elf_cals)
+
+    elf_cals.sort(reverse=True)
+    print(elf_cals)
+    return elf_cals[0]
