57643f39231690cf91221784f51cca659ee64fcd
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2022-12-05 11:55:16 -0500

    day 1 input file and get answer with it

--- day_01a.py
+++ day_01a.py
@@ -27,3 +27,6 @@ def elf_max_calories(fname):
 
 ans = elf_max_calories('test_01a.txt')
 print(f'Elf max calories is {ans} - should be 24000')
+
+ans = elf_max_calories('input_01a.txt')
+print(f'Elf max calories is {ans}')
